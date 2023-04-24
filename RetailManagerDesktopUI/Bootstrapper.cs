f290f8c7fbfbe3321be86578285426206182f5f0
Rahul Ram <rahulram30122000@gmail.com>
2023-04-24 22:00:13 +0530

    WPF added views binded

---
+++ RetailManagerDesktopUI/Bootstrapper.cs
@@ -0,0 +1,59 @@
+﻿using Caliburn.Micro;
+using RetailManagerDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RetailManagerDesktopUI
+{
+    public class Bootstrapper: BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        //wires up everything
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            //Singleton - Creates a one time instantiation which is used throughout the lifecycle of the app
+            //PerRequest - Create a new instance when it is called
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>(); //Raise event and manages it
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
